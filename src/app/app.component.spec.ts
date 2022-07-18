7017bf1c1898ef893872686537a215ad87e40f80
“Alex” <aflorentin1814@gmail.com>
2022-07-18 15:24:19 +0300

    ..

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'books-angular'`, () => {
+  it(`should have as title 'calc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('books-angular');
