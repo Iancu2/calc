9a4398b37fc95a577d4906de63e081a18efba4e1
“Alex” <aflorentin1814@gmail.com>
2022-07-18 15:43:58 +0300

    iancu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'books-angular'`, () => {
+  it(`should have as title 'calc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('books-angular');
+    expect(app.title).toEqual('calc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('books-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('calc app is running!');
   });
 });
